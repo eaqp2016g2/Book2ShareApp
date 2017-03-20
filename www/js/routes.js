139d7ca26adbf9a58c127f55c78e06af72597f3c
jonatancm10 <jonatancm10@gmail.com>
2017-03-20 13:50:49 +0100

    diseño app movil para iOS y Android

---
+++ www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.listTabDefaultPage', {
+    url: '/list',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/listTabDefaultPage.html',
+        controller: 'listTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.chatTabDefaultPage', {
+    url: '/chat',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/chatTabDefaultPage.html',
+        controller: 'chatTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.mapTabDefaultPage', {
+    url: '/map',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/mapTabDefaultPage.html',
+        controller: 'mapTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page6',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
