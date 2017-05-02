be639a6c3e84125b3d25c0904f13d76ca4dbf31e
Joan Jerez <joanjerezt@gmail.com>
2017-05-02 03:28:39 +0200

    mezcla a producción

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,7 @@
+var API = "http://localhost:3001/api/"
+
 angular.module('app.controllers', [])
-  
+
 .controller('listTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,7 +9,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('chatTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,7 +17,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('mapTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,7 +25,7 @@ function ($scope, $stateParams) {
 
 
 }])
-      
+
 .controller('bOOK2SHARECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,20 +33,58 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.login = function() {
+
+    $http({
+      url: API + 'users/login',
+      method: "POST",
+      data: $scope.newUser
+    })
+      .then(function (response) {
+          // success
+          if (response.data.success == true) {
+            localStorage.setItem("fs_app_token", response.data.token);
+            localStorage.setItem("fs_app_userdata", JSON.stringify(response.data.user));
+            window.location.reload();
+          }
+        },
+        function (response) { // optional
+          // failed
+          $ionicLoading.show({template: 'Ha fallat l\' inici de sessió', noBackdrop: true, duration: 2000});
+        });
+  }
 
 }])
-   
+
 .controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.registra = function() {
+    $http({
+      url: API + 'users/register',
+      method: "POST",
+      data: $scope.newUser
+    })
+      .then(function (response) {
+          // success
+          if (response.data.success == true) {
+            localStorage.setItem("fs_app_token", response.data.token);
+            localStorage.setItem("fs_app_userdata", JSON.stringify(response.data.user));
+            window.location.reload();
+          }
+        },
+        function (response) { // optional
+          // failed
+          $ionicLoading.show({template: 'Ha fallat el registre', noBackdrop: true, duration: 2000});
+        });
+  }
 
 }])
- 
