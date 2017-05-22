f8e111b241379a96c18e71e6fa5a6546c8abb311
jonatancm10 <jonatancm10@gmail.com>
2017-05-22 11:25:53 +0200

    trabajo avanzado

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,18 +2,14 @@ var API = "http://localhost:3001/api";
 
 angular.module('app.controllers', [])
 
-<<<<<<< HEAD
 .controller('listTabDefaultPageCtrl', ['$http','$scope', '$stateParams', '$rootScope', '$ionicPopup','$state', function ($http, $scope, $stateParams, $rootScope, $ionicPopup, $state) {
   $http({
-=======
-  .controller('listTabDefaultPageCtrl', ['$http', '$scope', '$stateParams', '$rootScope', function ($http, $scope, $stateParams, $rootScope) {
-    $http({
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
+
       url: API + '/books/user/' + $rootScope.userdata.name,
       method: "GET"
     })
       .then(function (response) {
-<<<<<<< HEAD
+
             $scope.books = response.data;
             $rootScope.book = JSON.parse(localStorage.getItem("fs_web_book"));
             console.log('Libros obtenidos para mi ' + $rootScope.book)
@@ -64,30 +60,7 @@ angular.module('app.controllers', [])
 }])
 
 .controller('chatTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-=======
-        $scope.books = response.data;
-        console.log('Libros obtenidos para mi' + $scope.books)
-      }, function (error) {
-        console.log('Error al obtener los libros: ' + error.data);
-      });
-    /* $scope.getBooks = function () {
-     $http({
-     url: API + '/book',
-     method: "GET"
-     })
-     .then(function (response) {
-     $scope.books = response.data;
-     console.log('Libros obtenidos para mi' +$scope.books)
-     }, function (error) {
-     console.log('Error al obtener los libros: ' + error.data);
-     });
 
-     }*/
-
-  }])
-
-  .controller('chatTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
     function ($scope, $stateParams) {
@@ -95,7 +68,6 @@ angular.module('app.controllers', [])
 
     }])
 
-<<<<<<< HEAD
 .controller('editBookCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -119,9 +91,6 @@ function ($scope, $stateParams) {
 }])
 
 .controller('mapTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-=======
-  .controller('mapTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
     function ($scope, $stateParams) {
@@ -144,7 +113,6 @@ function ($scope, $stateParams) {
         data: $scope.user
 
       })
-<<<<<<< HEAD
           .then(function (response) {
                   if (response.data.success == true) {
                       localStorage.setItem("fs_web_token", response.data.token);
@@ -164,48 +132,15 @@ function ($scope, $stateParams) {
               function (error) {
                   console.log(error);
                   console.log('Error al iniciar sessio' + error);
-=======
-        .then(function (response) {
-            if (response.data.success == true) {
-              localStorage.setItem("fs_web_token", response.data.token);
-              localStorage.setItem("fs_web_userdata", JSON.stringify(response.data.user));
-              //console.log('user2', $scope.userdata)
-              //$mdDialog.hide();
-              $rootScope.logged = true;
-              $rootScope.userdata = JSON.parse(localStorage.getItem("fs_web_userdata"));
-
-            } else {
-              var alertPopup = $ionicPopup.alert({
-                title: 'Login failed!',
-                template: 'Please check your credentials!'
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
               });
-
-              $state.go('login');
-              console.log("Ha fallat l'inici de sessió");
-            }
-          },
-          function (error) {
-            console.log(error);
-            console.log('Error al iniciar sessio' + error);
-          });
     };
     $scope.logout = function () {
       localStorage.removeItem("fs_web_token");
       localStorage.removeItem("fs_web_userdata");
-<<<<<<< HEAD
       $rootScope.userdata={};
       $rootScope.logged=false;
   };
 }])
-=======
-      $rootScope.userdata = {};
-      $rootScope.logged = false;
-    };
-
-  }])
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
-
   .controller('SignupCtrl', ['$http', '$state', '$scope', '$rootScope', function ($http, $state, $scope, $rootScope) {
     $http({
       url: API + '/users',
@@ -232,8 +167,9 @@ function ($scope, $stateParams) {
               } else {
                 console.log("Ha fallat l'inici de sessió");
               }
-<<<<<<< HEAD
-          };
+          });
+        }
+      }
 }])
 
 .controller('AddBookCtrl', [ '$http', '$state','$scope', '$rootScope', '$ionicPopup', function ($http, $state, $scope, $rootScope, $ionicPopup) {
@@ -261,43 +197,3 @@ function ($scope, $stateParams) {
                   });
           };
 }])
-=======
-            },
-            function (error) {
-              console.log('El correu electrònic està en ús' + error);
-            });
-      }
-      else {
-        console.log("Les contrasenyes no coincideixen");
-      }
-    };
-
-  }])
-
-  .controller('AddBookCtrl', ['$http', '$state', '$scope', '$rootScope', '$ionicPopup', function ($http, $state, $scope, $rootScope, $ionicPopup) {
-
-    $scope.newBook = {};
-    $scope.newBook.propietary = $rootScope.userdata.name
-    console.log('book', $scope.newBook)
-    $scope.addbook = function () {
-      $http({
-        url: API + '/book',
-        method: "POST",
-        data: $scope.newBook
-      })
-        .then(function (response) {
-          if (response.data.success == true) {
-            $scope.newBook = {};
-            var alertPopup = $ionicPopup.alert({
-              title: 'Libro añadido',
-              template: 'Libro añadido correctamente a tu biblioteca'
-            })
-            //$state.go("library")
-          } else {
-            console.log("Ha fallat la publicacio del llibre");
-          }
-        });
-    };
-
-  }]);
->>>>>>> 0bf11e840807bf4bd2c7da2188a67f3ded8fc492
