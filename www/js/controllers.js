87b800021e80ff3bb1e206ff6939a72874c31c77
jonatancm10 <jonatancm10@gmail.com>
2017-06-17 19:45:12 +0200

    chat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -88,11 +88,11 @@ angular.module('app.controllers', ['angular.filter'])
 
   }])
 
-  .controller('chatTabDefaultPageCtrl', ['$scope', '$stateParams', function ($scope, $stateParams, Chats) {
-    /*  $scope.chats = Chats.all();
-     $scope.remove = function(chat) {
-     Chats.remove(chat);
-     };*/
+  .controller('chatTabDefaultPageCtrl', ['$http','$scope', '$stateParams', '$Chats', function ($http, $scope, $stateParams, $Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+      Chats.remove(chat);
+    };
   }])
   .controller('chatDetailCtrl', ['$scope', '$stateParams', function ($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
